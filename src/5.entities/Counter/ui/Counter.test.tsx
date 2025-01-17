6f2f8b47ce6cc5b4c01ea2f14f6a7898703e5857
Larisa Zueva <larisa.zueva@mainsgroup.ru>
2025-01-17 20:36:00 +0300

    4.30 тест на userEvent Counter

--- src/5.entities/Counter/ui/Counter.test.tsx
+++ src/5.entities/Counter/ui/Counter.test.tsx
@@ -1,9 +1,10 @@
 import { fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Counter } from "./Counter";
 import { componentRender } from "6.shared/lib";
 
-describe("Sidebar", () => {
+describe("Counter", () => {
   test("test render", () => {
     componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
     screen.debug();
@@ -13,14 +14,15 @@ describe("Sidebar", () => {
     componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
     const incrementBtn = screen.getByTestId("increment-btn");
     screen.debug();
-    fireEvent.click(incrementBtn);
+    // fireEvent.click(incrementBtn);
+    userEvent.click(incrementBtn);
     expect(screen.getByTestId("counter-title")).toHaveTextContent("11");
   });
-  test("decrement", () => {
-    componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
-    screen.debug();
-    const decrementBtn = screen.getByTestId("decrement-btn");
-    fireEvent.click(decrementBtn);
-    expect(screen.getByTestId("counter-title")).toHaveTextContent("9");
-  });
+  // test("decrement", () => {
+  //   componentRender(<Counter />, { initialState: { counter: { value: 10 } } });
+  //   screen.debug();
+  //   const decrementBtn = screen.getByTestId("decrement-btn");
+  //   fireEvent.click(decrementBtn);
+  //   expect(screen.getByTestId("counter-title")).toHaveTextContent("9");
+  // });
 });
