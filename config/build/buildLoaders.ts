0f12cb65d93c2b581fe5548e13ee149bc79faecb
Larisa Zueva <larisa.zueva@mainsgroup.ru>
2025-03-12 17:59:49 +0300

    6.46 React refresh plugin. build babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -37,6 +37,9 @@ export function buildLoaders(options: IBuildOptions): webpack.RuleSetRule[] {
       loader: "babel-loader",
       options: {
         presets: ["@babel/preset-env"],
+        plugins: [isDev && require.resolve("react-refresh/babel")].filter(
+          Boolean
+        ),
       },
     },
   };
