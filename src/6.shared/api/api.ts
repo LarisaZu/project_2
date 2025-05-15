b994653d137dbf946755af6b2ae25b8948169d6e
Larisa Zueva <larisa.zueva@mainsgroup.ru>
2025-05-15 18:16:33 +0300

    fix apiUrl

--- src/6.shared/api/api.ts
+++ src/6.shared/api/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 import { USER_LOCALSTORAGE_KEY } from "6.shared/const/localstorage";
 
-console.log("🚀 ~ __API__:", __API__);
 export const $api = axios.create({
   baseURL: __API__,
   headers: { authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || "" },
